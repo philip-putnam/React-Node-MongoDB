3dbd099977305307827928dd68c24167fa663a4d
Philip Putnam <putnamp989@gmail.com>
2017-06-13 19:58:07 -0700

    setup server.js to utilize apiRouter, and express.static

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import config from './config';
 import apiRouter from './api';
+// import fs from 'fs';
 
 import express from 'express';
 const server = express();
@@ -8,9 +9,14 @@ server.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
-server.get('/about', (req, res) => {
-  res.send('The about page');
-});
+server.use('/api', apiRouter);
+server.use(express.static('./public'));
+
+// server.get('/about', (req, res) => {
+//   fs.readFile('./public/about.html', (err, data) => {
+//     res.send(data.toString());
+//   });
+// });
 
 server.listen(config.port, () => {
   console.info('Express listening on port', config.port);
