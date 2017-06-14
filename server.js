74d4a39fe7d6f0d7ef5404f2ff2ccb2803f902af
Philip Putnam <putnamp989@gmail.com>
2017-06-13 20:13:43 -0700

    add ejs templating

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ import apiRouter from './api';
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.render('index', {
+    content: 'Hello Express and EJS!'
+  });
 });
 
 server.use('/api', apiRouter);
