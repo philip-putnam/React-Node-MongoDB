f8d17517a1ccf7c175496421a5b18af1fef3c251
Philip Putnam <putnamp989@gmail.com>
2017-06-14 11:06:31 -0700

    add 'root' element for jsx/React to render, webpack build bundle.js, link build.js in footer.ejs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import config from './config';
 import apiRouter from './api';
-// import fs from 'fs';
 
 import express from 'express';
 const server = express();
@@ -9,18 +8,12 @@ server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
   res.render('index', {
-    content: 'Hello Express and EJS!'
+    content: '...'
   });
 });
 
 server.use('/api', apiRouter);
-server.use(express.static('./public'));
-
-// server.get('/about', (req, res) => {
-//   fs.readFile('./public/about.html', (err, data) => {
-//     res.send(data.toString());
-//   });
-// });
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
   console.info('Express listening on port', config.port);
