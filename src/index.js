298fe6cec2532723694a209cbc525727c5fff80d
Philip Putnam <putnamp989@gmail.com>
2017-06-14 11:30:37 -0700

    create Header.js, App.js files migrating index.js components into respective files

--- src/index.js
+++ src/index.js
@@ -2,30 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ message }) => {
-  return (
-    <h2 className='Header text-center'>
-      {message}
-    </h2>
-  );
-};
-
-Header.propTypes = {
-  message: React.PropTypes.string
-};
-
-const App = (props) => {
-  return (
-    <div className='App'>
-      <Header message="Naming Contests" />
-      <div>
-
-      </div>
-    </div>
-  );
-};
-
-
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
